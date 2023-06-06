e0b81cb4e1db807db399e794492c6ef4792b61ca
Peter <113168584+prawstho@users.noreply.github.com>
2023-06-06 12:51:09 -0230

    Added Event Logging

--- init.js
+++ init.js
@@ -18,9 +18,23 @@ myapp init --help
 // Node.js common core global modules
 const fs = require('fs');
 
+// Add logging to the CLI project by using eventLogging
+// load the logEvents module
+const logEvents = require('./logEvents');
+
+// define/extend an EventEmitter class
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {};
+
+// initialize an new emitter object
+const myEmitter = new MyEmitter();
+// add the listener for the logEvent
+myEmitter.on('log', (event, level, msg) => logEvents(event, level, msg));
+
 const myArgs = process.argv.slice(2);
 function initializeApp() {
     if(DEBUG) console.log('initializeApp()');
+    myEmitter.emit('log', 'init.initializeApp()', 'INFO', 'init option was called by CLI');
 
     switch (myArgs[1]) {
     case '--all':
@@ -39,6 +53,7 @@ function initializeApp() {
             if(error) throw error;              
             console.log(data.toString());
         });
+        myEmitter.emit('log', 'init.initializeApp()', 'INFO', 'invalid CLI option, usage displayed');
     }
 }
 
