6fe6c228534d47e561c2039a540a20b5b5a7841e
Peter <113168584+prawstho@users.noreply.github.com>
2023-06-15 10:13:47 -0230

    update a token

--- token.js
+++ token.js
@@ -111,6 +111,40 @@ function newToken(username) {
     return newToken.token;
 }
 
+function updateToken(argv) {
+    if(DEBUG) console.log('token.updateToken()');
+    if(DEBUG) console.log(argv);
+    fs.readFile(__dirname + '/json/tokens.json', 'utf-8', (error, data) => {
+        if(error) throw error; 
+        let tokens = JSON.parse(data);
+        tokens.forEach(obj => {
+            if(obj.username === argv[3]) {
+                if(DEBUG) console.log(obj);
+                switch (argv[2]) {
+                    case 'p':
+                    case 'P':
+                        obj.phone = argv[4];
+                        break;
+                    case 'e':
+                    case 'E':
+                        obj.email = argv[4];
+                        break;
+                    default:
+                }
+                if(DEBUG) console.log(obj);
+            }
+        });
+        userTokens = JSON.stringify(tokens);
+        fs.writeFile(__dirname + '/json/tokens.json', userTokens, (err) => {
+            if (err) console.log(err);
+            else { 
+                console.log(`Token record for ${argv[3]} was updated with ${argv[4]}.`);
+                myEmitter.emit('log', 'token.updateToken()', 'INFO', `Token record for ${argv[3]} was updated with ${argv[4]}.`);
+            }
+        })
+    });
+}
+
 function addDays(date, days) {
     var result = new Date(date);
     result.setDate(result.getDate() + days);
@@ -141,7 +175,7 @@ function tokenApp() {
             console.log('invalid syntax. node myapp token --upd [option] [username] [new value]')
             myEmitter.emit('log', 'token.updateToken() --upd', 'WARNING', 'invalid syntax, usage displayed');
         } else {
-//            updateToken(myArgs);
+            updateToken(myArgs);
         }
         break;
     case '--fetch':
